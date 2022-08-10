0c94476afc5b8874c01e30103558936893dc2d11
Orhan <Orhan_Demis@hotmail.de>
2022-08-10 15:55:12 +0200

    Changes

--- Creo Engine/Contracts/euch.sol
+++ Creo Engine/Contracts/euch.sol
@@ -12,52 +12,46 @@ import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
 
 
-
 contract EUCH is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIdCounter;
-
     mapping(address => bool) public isGovernance;
-    uint public GatchaMinLevel=0;
-    uint public GatchaFee=0;
-    string GlobalUri;
-    address ChestAddress;
-
-
-
-    mapping(uint256 => mapping(address => bool)) private _isNftExempt;
+    string public GlobalUri;
+    address public ChestAddress;
+    mapping(address => bool) public _isWalletExempt;
+    mapping(uint256 => mapping(address => bool)) public _isNftExempt;
 
     modifier onlyAdmin() {
-        // If the first argument of 'require' evaluates to 'false', execution terminates and all
-        // changes to the state and to Ether balances are reverted.
-        // This used to consume all gas in old EVM versions, but not anymore.
-        // It is often a good idea to use 'require' to check if functions are called correctly.
-        // As a second argument, you can also provide an explanation about what went wrong.
         require(isGovernance[msg.sender] == true, "Caller is not governance");
         _;
     }
 
-
     constructor() ERC721("EK Origin UN Character", "EUCH") {
-        isGovernance[msg.sender]=true;
-        GlobalUri="https://www.creoplay/nft/1/";
+        isGovernance[msg.sender] = true;
+        GlobalUri = "https://www.creoplay/nft/1/";
     }
 
+    function isWalletExempt(address wallet) public view returns (bool) {
+        return _isWalletExempt[wallet];
+    }
     function isNftExempt(uint256 tokenId, address wallet) public view returns (bool) {
         return _isNftExempt[tokenId][wallet];
     }
 
+    function setWalletExempt(address wallet, bool status) public onlyOwner{
+        require(_isWalletExempt[wallet] != status, "Wallet already in status");
+        _isWalletExempt[wallet] = status;
+    }
     function setNFTExempt(uint256 tokenId, address wallet, bool status) public onlyOwner{
         require(_isNftExempt[tokenId][wallet] != status, "NFT and wallet already in status");
-        _isNftExempt[tokenId][wallet]=status;
+        _isNftExempt[tokenId][wallet] = status;
     }
 
     function addGovernance(address addr) public onlyOwner{
-        isGovernance[addr]=true;
+        isGovernance[addr] = true;
     }
     function removeGovernance(address addr) public onlyOwner{
-        isGovernance[addr]=false;
+        isGovernance[addr] = false;
     }
 
     function isAdmin(address addr) public view returns (bool){
@@ -67,8 +61,8 @@ contract EUCH is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     function bulkMint(address to, uint amount) public onlyAdmin{
         for(uint i=0; i<amount; i++){
             uint256 tokenId = _tokenIdCounter.current()+1;
-            string memory tid=Strings.toString(tokenId);
-            string memory urii=string(abi.encodePacked(GlobalUri,tid));
+            string memory tid = Strings.toString(tokenId);
+            string memory urii = string(abi.encodePacked(GlobalUri,tid));
 
             _safeMint(to, tokenId);
             _setTokenURI(tokenId, urii);
@@ -77,14 +71,14 @@ contract EUCH is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     }
 
     function bulkTransfer(uint256[] memory tokenIds, address _to) public {
-        // uint256 length = tokenIds.length;
         for (uint256 i = 0; i < tokenIds.length; i++) {
             _transfer(msg.sender, _to, tokenIds[i]);
         }
     }
 
     function safeMint(address to, uint tipe) public {
-        IERC1155 ChestToken=IERC1155(ChestAddress);
+        require(ChestAddress != address(0),"Chest Address Not Set");
+        IERC1155 ChestToken = IERC1155(ChestAddress);
         require(ChestToken.balanceOf(to,tipe)>0,"No Key Available");
 
         ChestToken.safeTransferFrom(msg.sender, address(this),tipe, 1, "0x00");
@@ -97,26 +91,25 @@ contract EUCH is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
         _tokenIdCounter.increment();
     }
 
-    function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 value,
-        bytes calldata data
-    )external returns(bytes4){
-        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+
+    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
     }
 
-    // The following functions are overrides required by Solidity.
+    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory) public virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function onERC721Received(address, address, uint256, bytes memory) public virtual returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         override(ERC721, ERC721Enumerable)
     {
-        // if(_owner != _msgSender()){
-
-        // }
-        // require(_isNftExempt[tokenId][from],"Not Allowed To Transfer");
+        require(!_isNftExempt[tokenId][from],"NFT Is Not Allowed To Transfer");
+        require(!_isWalletExempt[from],"Wallet Is Not Allowed To Transfer");
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
@@ -125,6 +118,7 @@ contract EUCH is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     }
 
     function setChestAddress(address addr)public onlyAdmin{
+        require(addr!=address(0),"Empty Address");
         ChestAddress=addr;
     }
 
