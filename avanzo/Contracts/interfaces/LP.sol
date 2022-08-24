b4f7b4aa1f438f973745164e0cc944f75ab13857
Tushar Bhatia <36727099+tushar1698@users.noreply.github.com>
2022-08-24 17:36:34 +0530

    Add files via upload

---
+++ avanzo/Contracts/interfaces/LP.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+interface LP {
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
+    function sync() external;
+}
