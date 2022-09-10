0315be4ed805316623e61d080f83ceef6287f728
Orhan <Orhan_Demis@hotmail.de>
2022-09-10 08:56:27 +0200

    Hideaways

--- Hideways/Contracts/token/Token2.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.6 ;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Token2 is ERC20 {
-    constructor() ERC20("Token2", "TK2") public {}
-}
