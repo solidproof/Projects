6955b8a16f0294e761d0023220443436f9fb67a8
Orhan <Orhan_Demis@hotmail.de>
2022-09-10 09:18:56 +0200

    Changes

--- LOP/Contracts/swapContract.sol
+++ LOP/Contracts/swapContract.sol
@@ -3,7 +3,8 @@
 // Designed and Deployed by @crypt0xa
 // For more info, check out https://xa0.dev
 
-pragma solidity ^ 0.8 .0;
+// File: @openzeppelin/contracts/utils/Context.sol
+pragma solidity ^0.8.0;
 
 /**
  * @dev Provides information about the current execution context, including the
@@ -16,36 +17,36 @@ pragma solidity ^ 0.8 .0;
  * This contract is only required for intermediate, library-like contracts.
  */
 abstract contract Context {
-    function _msgSender() internal view virtual returns(address) {
+    function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
 
-    function _msgData() internal view virtual returns(bytes calldata) {
+    function _msgData() internal view virtual returns (bytes calldata) {
         return msg.data;
     }
 }
 
 library SafeMath {
 
-    function add(uint256 a, uint256 b) internal pure returns(uint256) {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
 
         return c;
     }
 
-    function sub(uint256 a, uint256 b) internal pure returns(uint256) {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         return sub(a, b, "SafeMath: subtraction overflow");
     }
 
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns(uint256) {
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b <= a, errorMessage);
         uint256 c = a - b;
 
         return c;
     }
 
-    function mul(uint256 a, uint256 b) internal pure returns(uint256) {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
         }
@@ -56,11 +57,11 @@ library SafeMath {
         return c;
     }
 
-    function div(uint256 a, uint256 b) internal pure returns(uint256) {
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return div(a, b, "SafeMath: division by zero");
     }
 
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns(uint256) {
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b > 0, errorMessage);
         uint256 c = a / b;
         assert(a == b * c + a % b); // There is no case in which this doesn't hold
@@ -69,13 +70,13 @@ library SafeMath {
     }
 
     // Only used in the  BondingCalculator.sol
-    function sqrrt(uint256 a) internal pure returns(uint c) {
+    function sqrrt(uint256 a) internal pure returns (uint c) {
         if (a > 3) {
             c = a;
-            uint b = add(div(a, 2), 1);
+            uint b = add( div( a, 2), 1 );
             while (b < c) {
                 c = b;
-                b = div(add(div(a, b), b), 2);
+                b = div( add( div( a, b ), b), 2 );
             }
         } else if (a != 0) {
             c = 1;
@@ -86,7 +87,7 @@ library SafeMath {
 
 
 // File: @openzeppelin/contracts/access/Ownable.sol
-pragma solidity ^ 0.8 .0;
+pragma solidity ^0.8.0;
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
@@ -118,7 +119,7 @@ abstract contract Ownable is Context {
     /**
      * @dev Returns the address of the current owner.
      */
-    function owner() public view virtual returns(address) {
+    function owner() public view virtual returns (address) {
         return _owner;
     }
 
@@ -161,9 +162,8 @@ abstract contract Ownable is Context {
 }
 
 interface IUniswapV2Router01 {
-    function factory() external pure returns(address);
-
-    function WETH() external pure returns(address);
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
 
     function addLiquidity(
         address tokenA,
@@ -174,8 +174,7 @@ interface IUniswapV2Router01 {
         uint amountBMin,
         address to,
         uint deadline
-    ) external returns(uint amountA, uint amountB, uint liquidity);
-
+    ) external returns (uint amountA, uint amountB, uint liquidity);
     function addLiquidityETH(
         address token,
         uint amountTokenDesired,
@@ -183,8 +182,7 @@ interface IUniswapV2Router01 {
         uint amountETHMin,
         address to,
         uint deadline
-    ) external payable returns(uint amountToken, uint amountETH, uint liquidity);
-
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
     function removeLiquidity(
         address tokenA,
         address tokenB,
@@ -193,8 +191,7 @@ interface IUniswapV2Router01 {
         uint amountBMin,
         address to,
         uint deadline
-    ) external returns(uint amountA, uint amountB);
-
+    ) external returns (uint amountA, uint amountB);
     function removeLiquidityETH(
         address token,
         uint liquidity,
@@ -202,8 +199,7 @@ interface IUniswapV2Router01 {
         uint amountETHMin,
         address to,
         uint deadline
-    ) external returns(uint amountToken, uint amountETH);
-
+    ) external returns (uint amountToken, uint amountETH);
     function removeLiquidityWithPermit(
         address tokenA,
         address tokenB,
@@ -213,8 +209,7 @@ interface IUniswapV2Router01 {
         address to,
         uint deadline,
         bool approveMax, uint8 v, bytes32 r, bytes32 s
-    ) external returns(uint amountA, uint amountB);
-
+    ) external returns (uint amountA, uint amountB);
     function removeLiquidityETHWithPermit(
         address token,
         uint liquidity,
@@ -223,51 +218,41 @@ interface IUniswapV2Router01 {
         address to,
         uint deadline,
         bool approveMax, uint8 v, bytes32 r, bytes32 s
-    ) external returns(uint amountToken, uint amountETH);
-
+    ) external returns (uint amountToken, uint amountETH);
     function swapExactTokensForTokens(
         uint amountIn,
         uint amountOutMin,
         address[] calldata path,
         address to,
         uint deadline
-    ) external returns(uint[] memory amounts);
-
+    ) external returns (uint[] memory amounts);
     function swapTokensForExactTokens(
         uint amountOut,
         uint amountInMax,
         address[] calldata path,
         address to,
         uint deadline
-    ) external returns(uint[] memory amounts);
-
+    ) external returns (uint[] memory amounts);
     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
-    external
-    payable
-    returns(uint[] memory amounts);
-
+        external
+        payable
+        returns (uint[] memory amounts);
     function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
-    external
-    returns(uint[] memory amounts);
-
+        external
+        returns (uint[] memory amounts);
     function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
-    external
-    returns(uint[] memory amounts);
-
+        external
+        returns (uint[] memory amounts);
     function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
-    external
-    payable
-    returns(uint[] memory amounts);
+        external
+        payable
+        returns (uint[] memory amounts);
 
-    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns(uint amountB);
-
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns(uint amountOut);
-
-    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns(uint amountIn);
-
-    function getAmountsOut(uint amountIn, address[] calldata path) external view returns(uint[] memory amounts);
-
-    function getAmountsIn(uint amountOut, address[] calldata path) external view returns(uint[] memory amounts);
+    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
 interface IUniswapV2Router02 is IUniswapV2Router01 {
@@ -278,8 +263,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
         uint amountETHMin,
         address to,
         uint deadline
-    ) external returns(uint amountETH);
-
+    ) external returns (uint amountETH);
     function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
         address token,
         uint liquidity,
@@ -288,7 +272,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
         address to,
         uint deadline,
         bool approveMax, uint8 v, bytes32 r, bytes32 s
-    ) external returns(uint amountETH);
+    ) external returns (uint amountETH);
 
     function swapExactTokensForTokensSupportingFeeOnTransferTokens(
         uint amountIn,
@@ -297,14 +281,12 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
         address to,
         uint deadline
     ) external;
-
     function swapExactETHForTokensSupportingFeeOnTransferTokens(
         uint amountOutMin,
         address[] calldata path,
         address to,
         uint deadline
     ) external payable;
-
     function swapExactTokensForETHSupportingFeeOnTransferTokens(
         uint amountIn,
         uint amountOutMin,
@@ -315,90 +297,90 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
 }
 
 interface IERC20 {
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns(uint256);
+  /**
+   * @dev Returns the amount of tokens in existence.
+   */
+  function totalSupply() external view returns (uint256);
 
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns(uint256);
+  /**
+   * @dev Returns the amount of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
 
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `recipient`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address recipient, uint256 amount) external returns(bool);
+  /**
+   * @dev Moves `amount` tokens from the caller's account to `recipient`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transfer(address recipient, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns(uint256);
+  /**
+   * @dev Returns the remaining number of tokens that `spender` will be
+   * allowed to spend on behalf of `owner` through {transferFrom}. This is
+   * zero by default.
+   *
+   * This value changes when {approve} or {transferFrom} are called.
+   */
+  function allowance(address owner, address spender) external view returns (uint256);
 
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns(bool);
+  /**
+   * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * IMPORTANT: Beware that changing an allowance with this method brings the risk
+   * that someone may use both the old and the new allowance by unfortunate
+   * transaction ordering. One possible solution to mitigate this race
+   * condition is to first reduce the spender's allowance to 0 and set the
+   * desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   *
+   * Emits an {Approval} event.
+   */
+  function approve(address spender, uint256 amount) external returns (bool);
+  
+  function decimals() external returns (uint8);
 
-    function decimals() external returns(uint8);
+  /**
+   * @dev Moves `amount` tokens from `sender` to `recipient` using the
+   * allowance mechanism. `amount` is then deducted from the caller's
+   * allowance.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Moves `amount` tokens from `sender` to `recipient` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(address sender, address recipient, uint256 amount) external returns(bool);
+  /**
+   * @dev Emitted when `value` tokens are moved from one account (`from`) to
+   * another (`to`).
+   *
+   * Note that `value` may be zero.
+   */
+  event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+  /**
+   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+   * a call to {approve}. `value` is the new allowance.
+   */
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-pragma solidity >= 0.7 .0 < 0.9 .0;
+pragma solidity >=0.7.0 <0.9.0;
 
-contract SwapContract is Ownable {
-    using SafeMath
-    for uint256;
+contract SwapContract is Ownable{
+    using SafeMath for uint256;
 
     address constant public BUSD = address(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
     address public LOP;
     IUniswapV2Router02 public uniswapV2Router;
     address public marketingWallet;
 
-    constructor(address _marketingWallet) {
+    constructor(address _marketingWallet) 
+    {
 
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
         uniswapV2Router = _uniswapV2Router;
@@ -406,13 +388,13 @@ contract SwapContract is Ownable {
 
     }
 
-    function setLOPAddress(address _token) public onlyOwner {
+    function setLOPAddress(address _token) public onlyOwner{
         LOP = _token;
     }
 
-    function updateMarketingWallet(address _newWallet) public onlyOwner {
+    function updateMarketingWallet(address _newWallet) public onlyOwner{
         marketingWallet = _newWallet;
-    }
+    }        
 
     function addLiquidity(uint256 tokenAmount, uint256 BUSDAmount) private {
         require(LOP != address(0x0), "LOP address not set");
@@ -433,21 +415,21 @@ contract SwapContract is Ownable {
         );
     }
 
-    function executeSwap(uint256 liquidityTokens, uint256 forMarketing, uint256 forLiquidity) public {
+    function executeSwap(uint256 liquidityTokens, uint256 forMarketing, uint256 forLiquidity) public{
         require(msg.sender == owner() || msg.sender == LOP, "Only owner or LOP ca can execute this.");
-        if (liquidityTokens > 0 && forLiquidity > 0) {
+        if(liquidityTokens > 0 && forLiquidity > 0){
             addLiquidity(liquidityTokens, forLiquidity);
         }
         IERC20(BUSD).transfer(marketingWallet, forMarketing);
 
     }
 
-    function emergenceRescueToken(address token) public onlyOwner {
+    function emergenceRescueToken(address token) public onlyOwner{
         IERC20(token).transfer(owner(), IERC20(token).balanceOf(address(this)));
     }
 
     //For emergency use in case we need to manually approve some IERC20 token
-    function approve(address token, address spender) public onlyOwner {
+    function approve(address token, address spender) public onlyOwner{
         IERC20(token).approve(spender, 100000000000000000000000000);
     }
 
