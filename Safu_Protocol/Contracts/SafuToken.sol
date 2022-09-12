d74032c928559006ac0e32e0abbf3b673d149e08
Mails Nielsen <m.nielsen@make-group.gmbh>
2022-09-12 13:09:08 +0200

    Changes

---
+++ Safu_Protocol/Contracts/SafuToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SafuToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Safu Protocol", "SAFU") {
+        _mint(msg.sender, initialSupply);
+    }
+}
