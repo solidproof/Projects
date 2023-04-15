588f419f685e65614c99cdc3b2323f4095692795
TusharBhatiaSP <120104639+TusharBhatiaSP@users.noreply.github.com>
2023-04-15 13:07:20 +0530

    Delete 0xDragon directory

--- 0xDragon/contracts/token/Fire.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-pragma solidity =0.8.18;
-
-import "./MintableToken.sol";
-
-contract Fire is MintableToken {
-    constructor() ERC20("Fire", "FIRE") {}
-}
