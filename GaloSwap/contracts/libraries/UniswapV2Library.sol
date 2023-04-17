e6b98ec221fdb54b972218cedcc1af1120945095
TusharBhatiaSP <120104639+TusharBhatiaSP@users.noreply.github.com>
2023-04-17 17:00:37 +0530

    Add files via upload

---
+++ GaloSwap/contracts/libraries/UniswapV2Library.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.6.6;
+
+import "./SafeMath.sol";
+
+import '../interfaces/IGaloPair.sol';
+
+library UniswapV2Library {
+    using SafeMath for uint;
+
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+        
+    }
+
+    // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
+    function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
+        require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
+        require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+        amountB = amountA.mul(reserveB) / reserveA;
+    }
+}
