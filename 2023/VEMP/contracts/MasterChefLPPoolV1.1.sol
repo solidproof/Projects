b1111c86842abf076c30d7dc9bea5a66f155cf8b
TusharBhatiaSP <120104639+TusharBhatiaSP@users.noreply.github.com>
2023-05-29 12:31:35 +0530

    Add files via upload

---
+++ 2023/VEMP/contracts/MasterChefLPPoolV1.1.sol
@@ -0,0 +1,258 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+
+// MasterChef is the master of VEMP. He can make VEMP and he is a fair guy.
+//
+// Note that it's ownable and the owner wields tremendous power. The ownership
+// will be transferred to a governance smart contract once VEMP is sufficiently
+// distributed and the community can show to govern itself.
+//
+// Have fun reading it. Hopefully it's bug-free. God bless.
+contract MasterChefLPPool is Initializable, OwnableUpgradeable {
+    using SafeMathUpgradeable for uint256;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of VEMPs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accVEMPPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accVEMPPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20Upgradeable lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. VEMPs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that VEMPs distribution occurs.
+        uint256 accVEMPPerShare; // Accumulated VEMPs per share, times 1e12. See below.
+    }
+
+    // The VEMP TOKEN!
+    IERC20Upgradeable public VEMP;
+    // admin address.
+    address public adminaddr;
+    // VEMP tokens created per block.
+    uint256 public VEMPPerBlock;
+    // Bonus muliplier for early VEMP makers.
+    uint256 public constant BONUS_MULTIPLIER = 1;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when VEMP mining starts.
+    uint256 public startBlock;
+    // withdraw status
+    bool public withdrawStatus;
+    // reward end status
+    bool public rewardEndStatus;
+    // reward end block number
+    uint256 public rewardEndBlock;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event RewardEndStatus(bool rewardStatus, uint256 rewardEndBlock);
+    event RewardPerBlock(uint256 oldRewardPerBlock, uint256 newRewardPerBlock);
+    event setAdmin(address oldAdminaddr, address newAdminaddr);
+    event setWithdrawStatus(bool withdrawStatus);
+
+    constructor() {}
+
+    function initialize(
+        IERC20Upgradeable _VEMP,
+        address _adminaddr,
+        uint256 _VEMPPerBlock,
+        uint256 _startBlock    
+    ) public initializer {
+        require(address(_VEMP) != address(0), "Invalid VEMP address");
+        require(address(_adminaddr) != address(0), "Invalid admin address");
+        
+        __Ownable_init();
+        VEMP = _VEMP;
+        adminaddr = _adminaddr;
+        VEMPPerBlock = _VEMPPerBlock;
+        startBlock = _startBlock;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(uint256 _allocPoint, IERC20Upgradeable _lpToken, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: lastRewardBlock,
+            accVEMPPerShare: 0
+        }));
+    }
+
+    // Update the given pool's VEMP allocation point. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+    
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public pure returns (uint256) {
+        if (_to >= _from) {
+            return _to.sub(_from).mul(BONUS_MULTIPLIER);
+        } else {
+            return _from.sub(_to);
+        }
+    }
+
+    // View function to see pending VEMPs on frontend.
+    function pendingVEMP(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accVEMPPerShare = pool.accVEMPPerShare;
+        uint256 rewardBlockNumber = block.number;
+        if(rewardEndStatus != false) {
+           rewardBlockNumber = rewardEndBlock;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, rewardBlockNumber);
+            uint256 VEMPReward = multiplier.mul(VEMPPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accVEMPPerShare = accVEMPPerShare.add(VEMPReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accVEMPPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward vairables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 rewardBlockNumber = block.number;
+        if(rewardEndStatus != false) {
+           rewardBlockNumber = rewardEndBlock;
+        }
+        
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, rewardBlockNumber);
+        uint256 VEMPReward = multiplier.mul(VEMPPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        pool.accVEMPPerShare = pool.accVEMPPerShare.add(VEMPReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterChef for VEMP allocation.
+    function deposit(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accVEMPPerShare).div(1e12).sub(user.rewardDebt);
+            safeVEMPTransfer(msg.sender, pending);
+        }
+        pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+        user.amount = user.amount.add(_amount);
+        user.rewardDebt = user.amount.mul(pool.accVEMPPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        require(withdrawStatus != true, "Withdraw not allowed");
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accVEMPPerShare).div(1e12).sub(user.rewardDebt);
+        safeVEMPTransfer(msg.sender, pending);
+        user.amount = user.amount.sub(_amount);
+        user.rewardDebt = user.amount.mul(pool.accVEMPPerShare).div(1e12);
+        pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Safe VEMP transfer function, just in case if rounding error causes pool to not have enough VEMPs.
+    function safeVEMPTransfer(address _to, uint256 _amount) internal {
+        uint256 VEMPBal = VEMP.balanceOf(address(this));
+        if (_amount > VEMPBal) {
+            VEMP.transfer(_to, VEMPBal);
+        } else {
+            VEMP.transfer(_to, _amount);
+        }
+    }
+    
+    // Update Reward per block
+    function updateRewardPerBlock(uint256 _newRewardPerBlock) public onlyOwner {
+        massUpdatePools();
+        emit RewardPerBlock(VEMPPerBlock, _newRewardPerBlock);
+        VEMPPerBlock = _newRewardPerBlock;
+    }
+
+    // Update withdraw status
+    function updateWithdrawStatus(bool _status) public onlyOwner {
+        require(withdrawStatus != _status, "Already same status");
+        emit setWithdrawStatus(withdrawStatus);
+        withdrawStatus = _status;
+    }
+
+    // Update reward end status
+    function updateRewardEndStatus(bool _status, uint256 _rewardEndBlock) public onlyOwner {
+        require(rewardEndStatus != _status, "Already same status");
+        rewardEndBlock = _rewardEndBlock;
+        rewardEndStatus = _status;
+        emit RewardEndStatus(_status, _rewardEndBlock);
+    }
+
+    // Update admin address by the previous admin.
+    function admin(address _adminaddr) public {
+        require(msg.sender == adminaddr, "admin: wut?");
+        emit setAdmin(adminaddr, _adminaddr);
+        adminaddr = _adminaddr;
+    }
+
+    // Safe VEMP transfer function to admin.
+    function emergencyWithdrawRewardTokens(address _to, uint256 _amount) public {
+        require(_to != address(0), "Invalid to address");
+        require(msg.sender == adminaddr, "sender must be admin address");
+        safeVEMPTransfer(_to, _amount);
+    }
+}
