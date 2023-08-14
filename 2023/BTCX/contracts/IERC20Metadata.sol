83d32688e202a1d6d924e21ffd3c1fcc911d4407
Orhan <Orhan_Demis@hotmail.de>
2023-08-14 18:24:25 +0200

    BTCX

---
+++ 2023/BTCX/contracts/IERC20Metadata.sol
@@ -0,0 +1,28 @@
+// File: IERC20Metadata.sol
+// SPDX-License-Identifier: MIT
+
+import "./IERC20.sol";
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
