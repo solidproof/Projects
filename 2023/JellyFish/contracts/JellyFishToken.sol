8f21daa03a318d7074866d507660d276005b1988
rajurajkishoreSP <124571527+rajurajkishoreSP@users.noreply.github.com>
2023-10-04 15:11:18 +0530

    Add files via upload

---
+++ 2023/JellyFish/contracts/JellyFishToken.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.2/security/Pausable.sol";
+import "@openzeppelin/contracts@4.9.2/access/Ownable.sol";
+
+/// @custom:security-contact dev@jellyfi.me
+/// @custom:dev-run-script
+contract JellyfishMobileToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Jellyfish Mobile Token", "JFISH") {
+        _mint(msg.sender, 10000000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
